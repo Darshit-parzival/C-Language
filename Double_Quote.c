1db1e7eceff190c8ab33b0f2cfe446466447bebb
Darshit Rakhasiya <71558362+Darshit-parzival@users.noreply.github.com>
2022-01-28 20:53:13 +0530

    Printf

---
+++ Double_Quote.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+
+void main()
+{
+    printf("Double Quote in C LAnguage");
+    printf("\n\"God is Great\"");
+}
---
+++ Personal_Details.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+void main()
+{
+    printf("Name: Darshit Rakhasiya");
+    printf("\nAddress: Jnd, Gj, In");
+    printf("\n City: Junagadh");
+}
